682cc2fe42817e7d5a2d4e56f69d2359f0d46ca3
Guilherme H. <ghantani@gmail.com>
2024-10-18 16:10:47 -0300

    Fix: "Banner" position

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner";
+import styles from './Home.module.css'
 
 function Home(){
     return(
         <>
             <Banner/>
+            <div className={styles.padding}></div>
         </>
     )
 }
