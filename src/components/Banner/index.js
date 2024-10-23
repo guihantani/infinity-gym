e659f89806b63dd16e70a5273983c8644803d6c5
Guilherme H. <ghantani@gmail.com>
2024-10-23 15:54:38 -0300

    Feat: change banner title color

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -7,7 +7,10 @@ function Banner(){
                 <source src='/videos/gym-video.webm' type='video/webm'/>
             </video>
             <div className={styles.banner__content}>
-                <h1>Infinity Gym</h1>
+                <div className={styles.banner__titulo}>
+                    <h1>Infinity </h1>
+                    <h1 className={styles.vermelho}>Gym</h1>
+                </div>
                 <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis euismod dolor, placerat tincidunt nulla. Sed varius at quam a egestas.</h2>
             </div>
         </section>
