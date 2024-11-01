7dafbdd219c691b7f4c5b0a542a714b1db55ab54
Guilherme H. <ghantani@gmail.com>
2024-11-01 16:17:57 -0300

    Add: 'SobrePrincipios' animations

--- src/components/SobrePrincipios/index.js
+++ src/components/SobrePrincipios/index.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import PrincipioCard from '../PrincipioCard'
 import styles from './SobrePrincipios.module.css'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function SobrePrincipios(){
+    useEffect(() =>{
+        let mm = gsap.matchMedia();
+        mm.add("(prefers-reduced-motion: no-preference)", () => {
+            gsap.registerPlugin(ScrollTrigger);
+
+            let tlText = gsap.timeline({
+                scrollTrigger:{
+                    trigger: (`.${styles.sobrePrincipios__container}`),
+                    start: 'top 85%',
+                    end: 'bottom 60%'
+                }
+            })
+
+            tlText.fromTo((`.${styles.sobrePrincipios__container} h1`),{opacity: 0, x:'-20%'}, {opacity: 1, x:'0%'})
+            tlText.fromTo((`.${styles.sobrePrincipios__principios}`),{opacity: 0, y:'30%'}, {opacity: 1, y:'0%', ease: "power4.out"})
+        });
+    })
+
     return(
         <section className={styles.sobrePrincipios}>
             <div className={styles.sobrePrincipios__container}>
