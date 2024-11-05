551c23a97a5284920117c431bc3f2d02b5b1ad24
Guilherme H. <ghantani@gmail.com>
2024-11-05 14:36:48 -0300

    Update: "Planos" Banner

--- src/pages/Planos/index.js
+++ src/pages/Planos/index.js
@@ -1,5 +1,6 @@
 import CardPlano from '../../components/CardPlano';
-import styles from './Planos.module.css'
+import styles from './Planos.module.css';
+import BannerGeral from '../../components/BannerGeral';
 
 function Planos(){
     const beneficiosBasicos = ['Academia disponível 24h', 'Avaliação Física']
@@ -8,10 +9,7 @@ function Planos(){
 
     return(
         <section className={styles.planos}>
-            <div className={styles.planos__texto}>
-                <h1>Planos</h1>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis euismod dolor, placerat tincidunt nulla. Sed varius at quam a egestas.</h2>
-            </div>
+            <BannerGeral titulo={'Planos'} texto={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis euismod dolor'} imagem={'/images/planos.jpg'}/>
             <div className={styles.planos__container}>
                 <CardPlano nomePlano='Plano Básico' precoPlano='R$ 79,99' descricaoPlano='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' beneficios={beneficiosBasicos}/>
                 <CardPlano nomePlano='Plano Premium' precoPlano='R$ 109,99' descricaoPlano='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' beneficios={beneficiosPremium} destaque/>
